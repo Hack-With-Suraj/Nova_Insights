ed99c1dcc47c44f00bb199fc196b06d4e78dddcb
Suraj Yadav <hackwithsuraj@gmail.com>
2025-03-18 12:51:56 +0530

    add some TRPC configuration

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,22 +1,67 @@
-import { initTRPC } from '@trpc/server';
-import { cache } from 'react';
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { ratelimit } from "@/lib/ratelimit";
+import { auth } from "@clerk/nextjs/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+import { cache } from "react";
+import superjson from "superjson";
+
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
-  return { userId: 'user_123' };
+
+  // NOTE : Its not a DataBase id it a Clerk UserId
+  const { userId } = await auth();
+  // console.log(userId);
+
+  return { Clerk_User_Id: userId };
 });
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
-  // transformer: superjson,
+  transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const ProtectedProcedure = t.procedure.use(async function isAuthed(
+  opts
+) {
+  const { ctx } = opts;
+  if (!ctx.Clerk_User_Id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.clerkId, ctx.Clerk_User_Id))
+    .limit(1);
+
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const { success } = await ratelimit.limit(user.id);
+  
+  if (!success) {
+    throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+  }
+  return opts.next({
+    ctx: {
+      ...ctx,
+      user,
+    },
+  });
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,17 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { createTRPCRouter, ProtectedProcedure } from "../init";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  hello: ProtectedProcedure.input(
+    z.object({
+      text: z.string(),
+    })
+  ).query((opts) => {
+    console.log(opts.ctx.user);
+
+    return {
+      greeting: `hello ${opts.input.text}`,
+    };
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
